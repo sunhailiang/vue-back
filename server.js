6cbd843ca5936552a0af9deb84167d60c1a99629
ËﺣÁÁ <sunhl@Fosun.com>
2020-04-20 17:52:20 +0800

    test

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 let http = require('http')
 let users = [
   {
-    id: 000,
+    id: 0000,
     name: '海亮'
   },
   {
-    id: 001,
+    id: 0001,
     name: '虚竹'
   },
   {
-    id: 002,
+    id: 0002,
     name: '乔峰'
   }
 ]
